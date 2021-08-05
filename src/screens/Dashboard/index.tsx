2fa46e5db54bf2d1e80a97cff164cb54b1cf6b05
João Gabriel Pancheski <jgabrielpancheski@gmail.com>
2021-08-05 20:42:26 -0300

    feat: dashboard header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/32856374?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>João Gabriel</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name='power' />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
