35c5812bd788b94a99d518f1b2ff2467c76b522a
João Gabriel Pancheski <jgabrielpancheski@gmail.com>
2021-08-05 21:37:57 -0300

    feat: highlight cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { HighlightCard } from '../../components/HighlightCard';
+
 import {
   Container,
   Header,
@@ -9,7 +11,8 @@ import {
   User,
   UserGreeting,
   UserName,
-  Icon
+  Icon,
+  HighlightCards
 } from './styles';
 
 export function Dashboard() {
@@ -30,6 +33,29 @@ export function Dashboard() {
           <Icon name='power' />
         </UserWrapper>
       </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type='up'
+          title='Entradas'
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+        />
+
+        <HighlightCard
+          type='down'
+          title='Saídas'
+          amount='R$ 1.259,00'
+          lastTransaction='Última saída dia 03 de abril'
+        />
+
+        <HighlightCard
+          type='total'
+          title='Total'
+          amount='R$ 16.141,00'
+          lastTransaction='01 à 16 de abril'
+        />
+      </HighlightCards>
     </Container>
   )
 }
